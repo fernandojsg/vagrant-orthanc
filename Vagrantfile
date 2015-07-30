154a2d21f6afc6cd0a24fcb7f0054a181e1598ff
Fernando Serrano <fernandojsg@gmail.com>
2015-07-30 02:08:30 +0200

    nginx

--- Vagrantfile
+++ Vagrantfile
@@ -40,6 +40,7 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
   # Create a forwarded port mapping which allows access to a specific port
   # within the machine from a port on the host machine. In the example below,
   # accessing "localhost:8080" will access port 80 on the guest machine.
+  config.vm.network "forwarded_port", guest: 80, host: 8043
   config.vm.network "forwarded_port", guest: 8042, host: 8042
   config.vm.network "forwarded_port", guest: 4242, host: 4242
   
--- puppet/manifests/default.pp
+++ puppet/manifests/default.pp
@@ -11,33 +11,26 @@ node default {
 
     Exec["apt-update"] -> Package <| |>
 
+    #include orthanc
+    #include orthancdicomweb
+    #include orthancwebviewer
+    #include orthancpostgresql
 
-    # Postgresql general config
-    class { 'postgresql::globals':
-        version             => "9.4",
-        manage_package_repo => true,
-        encoding             => "UTF8",
-        #locale              => "en_EN.UTF-8"
-    }
-
-    class { "postgresql::server":
-        # Postgresql server config
-        ip_mask_deny_postgres_user => '0.0.0.0/32',
-        ip_mask_allow_all_users    => '0.0.0.0/0',
-        listen_addresses           => '*',
-        ipv4acls                   => ['host all all 0.0.0.0/0 md5'],
-        postgres_password          => 'pgpassword',
-    }
-
-    postgresql::server::db { 'orthanc':
-        user     => 'pgorthancuser',
-        password => postgresql_password('pgorthancuser', 'pgorthancpass'),
-        before => Service['orthanc']
-    }
-
-    include orthanc
-    include orthancdicomweb
-    include orthancwebviewer
-    include orthancpostgresql
+    class { 'nginx': }
     
+    nginx::resource::vhost { 'localhost':
+        proxy => 'http://localhost:8042',
+        proxy_set_header => [ 
+            'HOST $host', 
+            'X-Real-IP $remote_addr' 
+        ],
+        add_header => {
+            'Access-Control-Allow-Origin' => '*',
+            'Access-Control-Allow-Credentials' => 'true',
+            'Access-Control-Allow-Methods' => 'GET, POST, OPTIONS',
+            'Access-Control-Allow-Headers' => 'DNT,X-CustomHeader,Keep-Alive,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type'
+        },
+        rewrite_rules => ['/orthanc(.*) $1 break']
+    }
+
 }
